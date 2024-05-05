907a72137603e1fd020992777c561df34d05c0cb
Yaseer161637 <yaseerhussainshaik@gmail.com>
2024-05-05 12:23:05 +0530

    #1 handled username validation

--- imdb/users/forms.py
+++ imdb/users/forms.py
@@ -1,7 +1,12 @@
+import os
+
 from wtforms import PasswordField, EmailField, StringField
 from wtforms.form import Form
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from pymongo import MongoClient
 
+client = MongoClient(os.environ.get('ENV_DB_URL'))
+db = client[os.environ.get('ENV_DB')]
 
 class LoginForm(Form):
     username = StringField('username', validators=[DataRequired()])
@@ -27,3 +32,7 @@ class RegisterForm(Form):
             DataRequired(), EqualTo('password', message='Passwords must match.')
         ]
     )
+
+    def validate_username(self, username_field):
+        if db.user.find_one({'username':username_field.data}):
+            raise ValidationError('Username already taken. Please choose a different one.')
